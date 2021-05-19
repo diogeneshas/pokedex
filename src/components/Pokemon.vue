36a69a47d8e08aeb72d1059af3a2bb62c3c58e3d
Diogenes <diogenes.henrique.br@gmail.com>
2021-05-19 01:37:07 -0300

    Melhorando componente

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -1,12 +1,49 @@
 <template>
   <div>
-      <h1>{{num}} {{name}}</h1>
-      <small>{{url}}</small>
+
+        <div class="card">
+        <div class="card-image">
+            <figure>
+            <img v-bind:src="pokemon.front" alt="Placeholder image">
+            </figure>
+        </div>
+        <div class="card-content">
+            <div class="media">
+            <div class="media-content">
+                <p class="title is-4">{{num}} {{name}}</p>
+                <p class="subtitle is-6">{{ pokemon.type }}</p>
+            </div>
+            </div>
+
+            <div class="content">
+
+            </div>
+        </div>
+        </div>
+
+
+
+
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+    created() {
+        axios.get(this.url).then(res => {
+            this.pokemon.type = res.data.types[0].type.name
+            this.pokemon.front = res.data.sprites.front_default
+            this.pokemon.back = res.data.sprites.back_default
+            console.log(this.pokemon)
+        })
+    },
+    data() {
+        return {
+            pokemon: []
+        }
+    },
     props: {
         num: Number,
         name: String,
